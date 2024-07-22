a822c0af6cf698e2b3257ece8ada91889d14d79a
root <root@WIN-S1BCOKSUEAO>
2024-07-22 19:35:46 +0800

    I update my sql files

---
+++ testmysql.sql
@@ -0,0 +1,18 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    echo "Connected succesfully";
+}
+echo "Connected successfully";
+?>
