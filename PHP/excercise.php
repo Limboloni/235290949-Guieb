637d2a8637b57b5f8a50a8e1f3f20e0430f04b7b
limboloni <pigeonie123@gmail.com>
2025-03-24 12:37:35 +0800

    I finished my php excercise

--- PHP/excercise.php
+++ PHP/excercise.php
@@ -8,90 +8,105 @@
 <body>
 
 <?php
-//exercise 1
+// Exercise 1: Variables and Operators
 echo "Exercise 1: Variables and Operators <br>";
 $a = 15;
 $b = 7;
-echo "<p>The sum of ".$a." and ". $b ."is ". ($a + $b) .".</p>";
-echo "<p>The difference of ". $a ." and ". $b ." is ". ($a - $b) .".</p>";
-echo "<p>The product of ". $a ." and ". $b ." is ". ($a * $b) .".</p>";
-echo "<p>The quotient of ". $a ." and ". $b ." is ". ($a / $b) .".</p>";
+$sum = $a + $b;
+$diff = $a - $b;
+$prod = $a * $b;
+$quot = $a / $b;
+
+echo "<p>The sum of $a and $b is $sum.</p>";
+echo "<p>The difference of $a and $b is $diff.</p>";
+echo "<p>The product of $a and $b is $prod.</p>";
+echo "<p>The quotient of $a and $b is $quot.</p>";
 ?>
 
 <?php
-//exercise 2
+// Exercise 2: Conditional Statements
 echo "Exercise 2: Conditional Statements <br>";
+
 $number = 10;
-echo "The number is ". $number . "<br>";
-if($number % 2 == 0){
-echo "The number is even. ";
-if($number > 0){
-echo "And the number is also positive";
+echo "The number is $number <br>";
+
+if ($number % 2 == 0) {
+    echo "The number is even. ";
+} else {
+    echo "The number is odd. ";
 }
-elseif($number < 0){
-echo "And the number is also negative";
+
+if ($number > 0) {
+    echo "<p>And the number is positive.</p>";
+} elseif ($number < 0) {
+    echo "<p>And the number is negative.</p>";
+} else {
+    echo "<p>And the number is zero.</p>";
 }
-elseif($number == 0){
-echo "And the number is also equal to 0";
-}
-}
-elseif($number % 2 != 0){
-echo "The number is odd. ";
-if($number > 0){
-echo "And the number is also positive";
-}
-elseif($number < 0){
-echo "And the number is also negative";
-}
-elseif($number == 0){
-echo "And the number is also equal to 0";
-}
-}
-echo"<br>";
-echo"<br>";
 ?>
 
 <?php
-//exercise 3
+// Exercise 3: Loops
 echo "Exercise 3: Loops <br>";
-
-echo "FizzBuzz:<br>";
 for ($i = 1; $i <= 100; $i++) {
-if($i % 3 == 0){
-if($i % 5 == 0){
-echo "FizzBuzz, ";
+    if ($i % 3 == 0 && $i % 5 == 0) {
+        echo "<p>FizzBuzz</p>";
+    } elseif ($i % 3 == 0) {
+        echo "<p>Fizz</p>";
+    } elseif ($i % 5 == 0) {
+        echo "<p>Buzz</p>";
+    } else {
+        echo "<p>$i</p>";
+    }
 }
-else{
-echo "Fizz, ";
-}
-}
-elseif($i % 5 == 0){
-if($i % 3 == 0){
-echo "FizzBuzz, ";
-}
-else{
-echo "Buzz, ";
-}
-}
-}
-echo"<br>";
-echo"<br>";
 
-echo "Fibonacci:<br>";
+$fib1 = 0;
+$fib2 = 1;
+$count = 0;
+echo "<p>Even Fibonacci numbers:</p>";
 
-
-echo"<br>";
-echo"<br>";
+while ($count < 10) {
+    $next = $fib1 + $fib2;
+    if ($next % 2 == 0) {
+        echo "<p>$next</p>";
+    }
+    $fib1 = $fib2;
+    $fib2 = $next;
+    $count++;
+}
 ?>
 
 <?php
-//exercise 4
-echo "Exercise 4: Functions <br>";
+// Exercise 4: Functions
+function greet($name) {
+    return "Hello, " . $name . "!";
+}
 
+function square($num) {
+    return $num * $num;
+}
 
+// Exercise 5: PHP script to display the submitted text
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $number = $_POST["number"];
 
+    if (!empty($name)) {
+        echo "<p>" . greet($name) . "</p>";
+    }
+
+    if (!empty($number)) {
+        echo "<p>The square of $number is: " . square($number) . "</p>";
+    }
+}
 ?>
 
+<!-- Exercise 5: Form Handling -->
+<form method="post">
+    Name: <input type="text" name="name">
+    Number: <input type="number" name="number">
+    <input type="submit" value="Submit">
+</form>
 
 </body>
 </html>
